88579f1b5809dba457518b7b6b4b8118671884e1
Nilesh Patel <npatel@comscore.com>
2019-03-10 14:10:09 +0530

    ready

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h2>React-hooks playground</h2>
+        <hr />
+      </div>
+    );
+  }
+}
+
+export default App;
